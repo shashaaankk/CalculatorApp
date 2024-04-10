de15b6961e3dc3cfb62188163f7745608593af93
Shashaaankk <shashaankbm@gmail.com>
2024-04-10 18:20:15 +0200

    first commit

---
+++ app/src/main/java/com/example/calculatorapp/CalculatorLogic.java
@@ -0,0 +1,12 @@
+package com.example.calculatorapp;
+
+import androidx.annotation.NonNull;
+
+public class CalculatorLogic {
+    public void CalculatorLogic(){}
+
+    public float calculate(String userIP)
+    {
+        return 0;
+    }
+}
